f6b0b4a56a197df7ded724f83e933361e7c41d71
Collins Masero <jnrmasero@gmail.com>
2016-10-28 07:08:29 -0700

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,9 @@
 """Print a Fibonacci sequence up to n."""
-import sys
-import os
 
-class Fibonacci:
-	def gen(self, n):
-         a, b = 0, 1
-         while b < n:
-             print b,
-             a, b = b, a+b
+def fib():
+    a,b = 1,1
+    num=eval(input("Enter Fib Nummber to be calculated: "))
+    num_int=int(num-2)
+    for i in range (num_int):
+        a,b=b,a+b
+    print(b)
