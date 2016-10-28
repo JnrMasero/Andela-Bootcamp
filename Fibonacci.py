8557e807aa597b18d0518a16243e31f876e1ce7e
Collins Masero <jnrmasero@gmail.com>
2016-10-28 06:59:12 -0700

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,9 +1,9 @@
+"""Print a Fibonacci sequence up to n."""
 import sys
 import os
 
 class Fibonacci:
 	def gen(self, n):
-         """Print a Fibonacci series up to n."""
          a, b = 0, 1
          while b < n:
              print b,
